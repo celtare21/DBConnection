796eb5bf4eda6cd4c2e723e9f18b924a08c54bae
Kuran Kaname <celtare21@gmail.com>
2020-10-29 22:56:08 +0200

    Update

--- Program.cs
+++ Program.cs
@@ -11,9 +11,6 @@ namespace DBConnection
 {
     class Program
     {
-        private static ExcelWorksheet Worksheet;
-        private static ExcelFile LoadedFile;
-
         [STAThread]
         static void Main()
         {
@@ -21,12 +18,21 @@ namespace DBConnection
 
             SpreadsheetInfo.SetLicense("FREE-LIMITED-KEY");
 
+            CheckForInternetConnection();
+
             using (var conn = new SqlConnection(connStr))
             {
-                SaveAllTables(conn);
-            }
+                /*string user_name;
 
-            Console.ReadLine();
+                Console.WriteLine("Enter your name:");
+
+                user_name = Console.ReadLine();
+
+                SaveAllTables(conn, user_name);*/
+
+                SaveAllTables(conn);
+                DeleteLastMonthE(conn);
+            }
         }
 
         private static List<string> GetAllTables(SqlConnection conn)
@@ -44,7 +50,8 @@ namespace DBConnection
                     {
                         string value = reader.GetValue(0).ToString();
 
-                        tables.Add(value);
+                        if (value.Contains("prezenta."))
+                            tables.Add(value);
                     }
                 }
             }
@@ -54,7 +61,7 @@ namespace DBConnection
             return tables;
         }
 
-        private static Dictionary<string, List<object>> GetAllElements(SqlConnection conn, string table)
+        private static Dictionary<string, List<object>> GetAllElementsLM(SqlConnection conn, string table)
         {
             string query;
             string[] columns = { "id", "date", "ora_incepere", "ora_final", "curs_alocat", "pregatire_alocat", "recuperare_alocat", "total" };
@@ -66,7 +73,8 @@ namespace DBConnection
             {
                 dic.Add(elem, new List<object>());
 
-                query = $@"SELECT {elem} FROM ""{table}""";
+                query = $@"SELECT {elem} FROM ""{table}"" WHERE date >= DATEADD(MONTH, DATEDIFF(MONTH, 0, GETDATE()) - 1, 0) AND date < DATEADD(MONTH, DATEDIFF(MONTH, 0, GETDATE()), 0)";
+                //query = $@"SELECT {elem} FROM ""{table}""";
 
                 using (var command = new SqlCommand(query, conn))
                 {
@@ -83,6 +91,19 @@ namespace DBConnection
             conn.Close();
 
             return dic;
+        }
+
+        private static void DeleteLastMonthE(SqlConnection conn)
+        {
+            foreach (var table_name in GetAllTables(conn))
+            {
+                string query = $@"DELETE FROM ""{table_name}"" WHERE date >= DATEADD(MONTH, DATEDIFF(MONTH, 0, GETDATE()) - 1, 0) AND date < DATEADD(MONTH, DATEDIFF(MONTH, 0, GETDATE()), 0)";
+
+                using (var command = new SqlCommand(query, conn))
+                {
+                    ExecuteCommandDB(command, conn);
+                }
+            }
         }
 
         private static void ExecuteCommandDB(SqlCommand command, SqlConnection conn)
@@ -113,13 +134,18 @@ namespace DBConnection
             }
         }
 
-        private static void SaveAllTables(SqlConnection conn)
+        private static void SaveAllTables(SqlConnection conn, string user_name = "")
         {
             string folder = OpenFolder();
 
             foreach (var table_name in GetAllTables(conn))
             {
-                var local_table = GetAllElements(conn, table_name);
+                if (!String.IsNullOrEmpty(user_name) && !table_name.Contains(user_name.ToLowerInvariant()))
+                    continue;
+
+                var local_table = GetAllElementsLM(conn, table_name);
+
+                Console.WriteLine(table_name);
 
                 SaveTable(local_table, folder, table_name);
             }
@@ -127,15 +153,17 @@ namespace DBConnection
 
         private static void SaveTable(Dictionary<string, List<object>> table, string path, string tableName)
         {
+            ExcelFile loadedFile;
+            ExcelWorksheet worksheet;
             Table tableMain, tableLittle;
             var zero = TimeSpan.FromHours(0);
             var total = (time: zero, curs: zero, pregatire: zero, recuperare: zero);
             int max = table["id"].Count(), j = 0;
 
-            LoadedFile = new ExcelFile();
-            Worksheet = LoadedFile.Worksheets.Add("Tables");
+            loadedFile = new ExcelFile();
+            worksheet = loadedFile.Worksheets.Add("Tables");
 
-            PopulateTables(max);
+            PopulateTables(worksheet, max);
 
             foreach (var key in table.Keys)
             {
@@ -146,10 +174,10 @@ namespace DBConnection
                     switch (key)
                     {
                         case "id":
-                            Worksheet.Cells[i + 1, j].Value = ConversionWrapper<int>((int)table[key][i]);
+                            worksheet.Cells[i + 1, j].Value = ConversionWrapper<int>((int)table[key][i]);
                             break;
                         case "date":
-                            Worksheet.Cells[i + 1, j].Value = ConversionWrapper<string>((DateTime)table[key][i]);
+                            worksheet.Cells[i + 1, j].Value = ConversionWrapper<string>((DateTime)table[key][i]);
                             break;
                         case "ora_incepere":
                         case "ora_final":
@@ -158,7 +186,7 @@ namespace DBConnection
                         case "recuperare_alocat":
                         case "total":
                             var localTime = (TimeSpan)table[key][i];
-                            Worksheet.Cells[i + 1, j].Value = ConversionWrapper<string>(localTime);
+                            worksheet.Cells[i + 1, j].Value = ConversionWrapper<string>(localTime);
                             _total += localTime;
                             break;
                         default:
@@ -191,32 +219,32 @@ namespace DBConnection
                     pregatire: GetIndice(total.pregatire) * Constant.pret_pregatire,
                     recuperare: GetIndice(total.recuperare) * Constant.pret_recuperare);
 
-            Worksheet.Cells[max + Constant.offset, 1].Value = TransformOverHour(total.time);
-            Worksheet.Cells[max + Constant.offset + 1, 1].Value = TransformOverHour(total.curs);
-            Worksheet.Cells[max + Constant.offset + 2, 1].Value = TransformOverHour(total.pregatire);
-            Worksheet.Cells[max + Constant.offset + 3, 1].Value = TransformOverHour(total.recuperare);
-            Worksheet.Cells[max + Constant.offset + 1, 2].Value = Constant.pret_curs;
-            Worksheet.Cells[max + Constant.offset + 2, 2].Value = Constant.pret_pregatire;
-            Worksheet.Cells[max + Constant.offset + 3, 2].Value = Constant.pret_recuperare;
-            Worksheet.Cells[max + Constant.offset + 1, 3].Value = GetIndice(total.curs);
-            Worksheet.Cells[max + Constant.offset + 2, 3].Value = GetIndice(total.pregatire);
-            Worksheet.Cells[max + Constant.offset + 3, 3].Value = GetIndice(total.recuperare);
-            Worksheet.Cells[max + Constant.offset + 1, 4].Value = valoare.curs;
-            Worksheet.Cells[max + Constant.offset + 2, 4].Value = valoare.pregatire;
-            Worksheet.Cells[max + Constant.offset + 3, 4].Value = valoare.recuperare;
-            Worksheet.Cells[max + Constant.offset + 4, 4].Value = valoare.curs + valoare.pregatire + valoare.recuperare;
+            worksheet.Cells[max + Constant.offset, 1].Value = TransformOverHour(total.time);
+            worksheet.Cells[max + Constant.offset + 1, 1].Value = TransformOverHour(total.curs);
+            worksheet.Cells[max + Constant.offset + 2, 1].Value = TransformOverHour(total.pregatire);
+            worksheet.Cells[max + Constant.offset + 3, 1].Value = TransformOverHour(total.recuperare);
+            worksheet.Cells[max + Constant.offset + 1, 2].Value = Constant.pret_curs;
+            worksheet.Cells[max + Constant.offset + 2, 2].Value = Constant.pret_pregatire;
+            worksheet.Cells[max + Constant.offset + 3, 2].Value = Constant.pret_recuperare;
+            worksheet.Cells[max + Constant.offset + 1, 3].Value = GetIndice(total.curs);
+            worksheet.Cells[max + Constant.offset + 2, 3].Value = GetIndice(total.pregatire);
+            worksheet.Cells[max + Constant.offset + 3, 3].Value = GetIndice(total.recuperare);
+            worksheet.Cells[max + Constant.offset + 1, 4].Value = valoare.curs;
+            worksheet.Cells[max + Constant.offset + 2, 4].Value = valoare.pregatire;
+            worksheet.Cells[max + Constant.offset + 3, 4].Value = valoare.recuperare;
+            worksheet.Cells[max + Constant.offset + 4, 4].Value = valoare.curs + valoare.pregatire + valoare.recuperare;
 
             if (max > 0)
             {
-                tableMain = Worksheet.Tables.Add("Table", $"A1:H{max + 1}", true);
+                tableMain = worksheet.Tables.Add("Table", $"A1:H{max + 1}", true);
                 tableMain.BuiltInStyle = BuiltInTableStyleName.TableStyleMedium2;
-                tableLittle = Worksheet.Tables.Add("TableLittle", $"A{max + Constant.offset + 1}:E{max + Constant.offset + 4}", true);
+                tableLittle = worksheet.Tables.Add("TableLittle", $"A{max + Constant.offset + 1}:E{max + Constant.offset + 4}", true);
                 tableLittle.BuiltInStyle = BuiltInTableStyleName.TableStyleMedium2;
             }
 
             try
             {
-                LoadedFile.Save($@"{path}\{tableName}.xlsx");
+                loadedFile.Save($@"{path}\{tableName}.xlsx");
             }
             catch (System.IO.IOException)
             {
@@ -243,35 +271,35 @@ namespace DBConnection
             return null;
         }
 
-        private static void PopulateTables(int max)
+        private static void PopulateTables(ExcelWorksheet worksheet, int max)
         {
-            Worksheet.Columns[0].SetWidth(135, LengthUnit.Pixel);
-            Worksheet.Columns[1].SetWidth(100, LengthUnit.Pixel);
-            Worksheet.Columns[2].SetWidth(100, LengthUnit.Pixel);
-            Worksheet.Columns[3].SetWidth(90, LengthUnit.Pixel);
-            Worksheet.Columns[4].SetWidth(90, LengthUnit.Pixel);
-            Worksheet.Columns[5].SetWidth(110, LengthUnit.Pixel);
-            Worksheet.Columns[6].SetWidth(120, LengthUnit.Pixel);
-            Worksheet.Columns[7].SetWidth(90, LengthUnit.Pixel);
+            worksheet.Columns[0].SetWidth(135, LengthUnit.Pixel);
+            worksheet.Columns[1].SetWidth(100, LengthUnit.Pixel);
+            worksheet.Columns[2].SetWidth(100, LengthUnit.Pixel);
+            worksheet.Columns[3].SetWidth(90, LengthUnit.Pixel);
+            worksheet.Columns[4].SetWidth(90, LengthUnit.Pixel);
+            worksheet.Columns[5].SetWidth(110, LengthUnit.Pixel);
+            worksheet.Columns[6].SetWidth(120, LengthUnit.Pixel);
+            worksheet.Columns[7].SetWidth(90, LengthUnit.Pixel);
 
-            Worksheet.Cells[0, 0].Value = "ID";
-            Worksheet.Cells[0, 1].Value = "Data";
-            Worksheet.Cells[0, 2].Value = "Ora incepere";
-            Worksheet.Cells[0, 3].Value = "Ora sfarsit";
-            Worksheet.Cells[0, 4].Value = "Curs alocat";
-            Worksheet.Cells[0, 5].Value = "Pregatire alocat";
-            Worksheet.Cells[0, 6].Value = "Recuperare alocat";
-            Worksheet.Cells[0, 7].Value = "Ora total";
+            worksheet.Cells[0, 0].Value = "ID";
+            worksheet.Cells[0, 1].Value = "Data";
+            worksheet.Cells[0, 2].Value = "Ora incepere";
+            worksheet.Cells[0, 3].Value = "Ora sfarsit";
+            worksheet.Cells[0, 4].Value = "Curs alocat";
+            worksheet.Cells[0, 5].Value = "Pregatire alocat";
+            worksheet.Cells[0, 6].Value = "Recuperare alocat";
+            worksheet.Cells[0, 7].Value = "Ora total";
 
-            Worksheet.Cells[max + Constant.offset, 0].Value = "TOTAL:";
-            Worksheet.Cells[max + Constant.offset + 1, 0].Value = "TOTAL CURS:";
-            Worksheet.Cells[max + Constant.offset + 2, 0].Value = "TOTAL PREGATIRE:";
-            Worksheet.Cells[max + Constant.offset + 3, 0].Value = "TOTAL RECUPERARE:";
-            Worksheet.Cells[max + Constant.offset, 2].Value = "PRET/H";
-            Worksheet.Cells[max + Constant.offset, 3].Value = "INDICE";
-            Worksheet.Cells[max + Constant.offset, 4].Value = "VALOARE";
+            worksheet.Cells[max + Constant.offset, 0].Value = "TOTAL:";
+            worksheet.Cells[max + Constant.offset + 1, 0].Value = "TOTAL CURS:";
+            worksheet.Cells[max + Constant.offset + 2, 0].Value = "TOTAL PREGATIRE:";
+            worksheet.Cells[max + Constant.offset + 3, 0].Value = "TOTAL RECUPERARE:";
+            worksheet.Cells[max + Constant.offset, 2].Value = "PRET/H";
+            worksheet.Cells[max + Constant.offset, 3].Value = "INDICE";
+            worksheet.Cells[max + Constant.offset, 4].Value = "VALOARE";
 
-            Worksheet.Cells[max + Constant.offset + 4, 3].Value = "TOTAL ORE:";
+            worksheet.Cells[max + Constant.offset + 4, 3].Value = "TOTAL ORE:";
         }
 
         private static T ConversionWrapper<T>(object elem)
@@ -289,57 +317,34 @@ namespace DBConnection
             }
         }
 
+        private static void CheckForInternetConnection()
+        {
+            try
+            {
+                using (var client = new System.Net.WebClient())
+                {
+                    using (client.OpenRead("http://google.com/generate_204"))
+                    {
+                        return;
+                    }
+                }
+            }
+            catch
+            {
+                Console.WriteLine("No internet connection!");
+                Console.ReadLine();
+                Environment.Exit(0);
+            }
+        }
+
         private static double GetIndice(TimeSpan time) =>
                 (DateTime.Parse(time.ToString(@"hh\:mm")) - DateTime.Parse("00:00")).TotalHours;
 
         private static string TransformOverHour(TimeSpan span) =>
                 $"{(int)span.TotalHours}:{span:mm}";
-    }
 
-    public class TableColumns
-    {
-        public string Date { get; }
-        public TimeSpan OraIncepere { get; }
-        public TimeSpan OraFinal { get; }
-        public TimeSpan CursAlocat { get; }
-        public TimeSpan PregatireAlocat { get; }
-        public TimeSpan RecuperareAlocat { get; }
-        public TimeSpan Total { get; }
-
-        public TableColumns(string date, TimeSpan oraIncepere, TimeSpan oraFinal, TimeSpan cursAlocat, TimeSpan pregatireAlocat, TimeSpan recuperareAlocat, TimeSpan total)
-        {
-            Date = date;
-            OraIncepere = oraIncepere;
-            OraFinal = oraFinal;
-            CursAlocat = cursAlocat;
-            PregatireAlocat = pregatireAlocat;
-            RecuperareAlocat = recuperareAlocat;
-            Total = total;
-        }
-    }
-
-    public class ExecuteFailure : Exception
-    {
-        public ExecuteFailure(string message)
-        {
-            Console.WriteLine(message);
-        }
-    }
-
-    public class HoursOutOfBounds : Exception
-    {
-        public HoursOutOfBounds()
-        {
-            Console.WriteLine("Hours out of bounds!");
-        }
-    }
-
-    public class AllParametersFalse : Exception
-    {
-        public AllParametersFalse()
-        {
-            Console.WriteLine("All parameters are false!");
-        }
+        private static string RemoveWhitespace(string str) =>
+                string.Join("", str.Split(default(string[]), StringSplitOptions.RemoveEmptyEntries));
     }
 
     public static class Constant
